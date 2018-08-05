548dccaa99cb177115ac53cf273a7eb7952a5cea
Edward Nolan <nolanedward@sbcglobal.net>
2018-08-05 17:06:41 -0400

    Add files via upload

--- pow.js
+++ pow.js
@@ -1,20 +1,52 @@
-function mineblock(difficulty){
-	while(this.Hash.substring(0, difficulty) !== Array(difficulty +1).join("0")){
-		this.nnonce++;
-		data = 777 ;
-		this.Hash = this.POW();
-		while (false);
-		var powhash = 0
-		powhash = this.Hash
-		console.log("Block Mined:", powhash);
-	}
+// Copyright (C) 2018 EdwardNolan, Lakewood OH 44107, nolanedward@sbcglobal.net
+// All rights reserved.   7/30/18
+
+// Each player has the ability to CPU mine additional coins if the player wagers 7x in a hand.
+
+
+	function mineblock() {
+	document.getElementById("stopBlockchain").style.display = 'none';
+	document.getElementById("text").innerHTML = "CPU Mined: ";
+	document.getElementById("CPU").innerHTML = "CPU REWARD: .2 COINS!";
+	var Reward=.2;
+    nonce = nonce + 1;
+	nnonce++;
+	data = 7 ;
+	var myDate = new Date();                     // console.log(myDate);     // 2018-06-05T02:15:19.213Z
+    timestamp = Math.round(+new Date()/1000);    // console.log(timestamp);  // 1528164919 this is seconds, not miliseconds
+	hash = (index + data + timestamp + previoushash + nnonce + nonce + sum);  //	console.log("new hash number of Hash =", hash);  // = 780410548299 is the Genesis numeric for Genesis hash = b5b417f04b
+	Hash = hash.toString(16);                    // console.log("New Hash =", Hash);  // New String of Number Hash = b5b417f04b
+	block = {transaction: (transaction), timestamp: (timestamp), data: (data), Hash: (Hash), previoushash: (previoushash), nonce: (nonce+1), confirmed: (confirmed), playersWallet: (WalletAddress), PvPCoins: (Reward)}; // New Block Object
+	blocks.push(block);
+    previoushash = hash
+	console.log(blocks);
+    miningReward = (miningReward + Reward)  // console.log(miningReward);
+  document.getElementById("unconfirmed").innerHTML = miningReward;
+  document.getElementById("transactions").innerHTML = Hash;
+  document.getElementById("rew").innerHTML = "You received " + Reward + " coins.";
+  Reward = 0;
+  data = 0 ;
 }
 
+	//Is new block valid with prev block? Check index, and compare hash of previous block
+	function isChainValid() {
+		for(let validate=1; validate < this.blocks.length; validate++){
+			const block = this.blocks[validate];
+			const blocks = this.blocks[validate - 1];
+			
+	if (blocks.index+1 != block.index) {;
+		console.log('invalid index');
+		return false;
+	}
+	else if (blocks.Hash != block.previoushash) {;   // Im not sure if these 3 if statements are working
+		console.log('invalid previoushash');
+		return false;
+	}
+	else if (hash(block) != block.hash) {;
+		return false;
+	}
+	}
+	}
 
-// this is a run away train, lol. It works with the var difficulty found on pvpcoin.js page. I had to set the dif high
-// in order to slow it down. Part of the problem here too though is that the game state doesnt interact correctly
-// with the dom if I call the function after and id is suppose to be updated and displayed. Oh well, it was fun for
-// now and will look at it again in the future or maybe canada can hanle it, lol.
 
-// I also created a new function POW() on on pvpcoin.js for testing this above code.
-// When I would call POW() from within game.js it worked but wouldnt allow the wager to hit the board.
+
